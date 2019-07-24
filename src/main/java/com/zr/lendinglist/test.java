729c85c177aced9e98abb6118879cbc20f870dc4
3103002128@qq.com <Micheal1024>
2019-07-24 21:09:21 +0800

    删掉功能代码

--- src/main/java/com/zr/lendinglist/test.java
+++
@@ -1,7 +0,0 @@
-package com.zr.lendinglist;
-
-/**
- * Created by Administrator on 2019/6/21.
- */
-public class test {
-}
--- src/main/java/com/zr/sourceofinput/Test.java
+++
@@ -1,7 +0,0 @@
-package com.zr.sourceofinput;
-
-/**
- * Created by Administrator on 2019/6/21 0021.
- */
-public class Test {
-}
